19d9d36a8622b7ab9f3ea6f0b6305f2d134c1df2
Dallan Simper <dallan.simper@gmail.com>
2024-11-15 09:19:41 -0700

    Extension methods

--- FileAccess/Program.cs
+++ FileAccess/Program.cs
@@ -11,45 +11,56 @@ internal class Program
 
 	private static void Main(string[] args)
 	{
-		NumberDelegate fun1 = AddOne;
-		NumberDelegate g = (x => x*2);
+		DateTime now = DateTime.UtcNow;
+		Console.WriteLine(now.ToSqlDate());
 
-		int b = Bar(76, fun1);
-		int b2 = Bar2(76, AddOne);
-		Bar3(5, PrintInt);
-		Bar4(7, IsPrime);
+		var items = new List<int> { 67, 345, 5, 6, 87, 9, 3, 43, 4, 34, 35 };
+		var ie = items.ApplyMethod(i => i - 23);
 
-		Console.WriteLine(b);
+		ie = ie.OrderBy(x => x % 5, "asc");
+
+		//ie.ToList().ForEach(i => Console.Write(i + ", "));
+
+
+		var people = new List<Person> {
+			new Person("Ethan", "Hintze", 2004),
+			new Person("Tyson", "Meade", 2003)
+			};
+
+		var sorted = people.OrderBy(p => DateTime.UtcNow.Year - p.BirthYear, "desc");
+		sorted.ToList().ForEach(p => Console.Write(p.FirstName + ", " + p.BirthYear + "; "));
 	}
-
-	private static int Bar(int c1, NumberDelegate fun){
-		return fun.Invoke(c1);
-	}
-
-	private static int Bar2(int c1, Func<int, int> fun){
-		return fun.Invoke(c1);
-	}
-	private static void Bar3(int c1, Action<int> fun){
-
-	}
-	private static void Bar4(int a, Func<int, bool> fun){
-	}
-	private static void Bar5(int a, Predicate<int> fun){
-
-	}
-
-	private static int AddOne(int a)
-	{
-		return a + 1;
-	}
-
-	private static void PrintInt(int a){
-		Console.WriteLine(a);
-	}
-
-	private static bool IsPrime(int a){
-		return false;
-	}
-
 }
 
+
+public record Person(string FirstName, string LastName, int BirthYear);
+
+public static class DateTimeExtensions
+{
+	public static string ToSqlDate(this DateTime date)
+	{
+		return date.ToString("yyyy-MM-dd");
+	}
+
+	public static IEnumerable<T> OrderBy(this IEnumerable<T> list,
+		Func<Person, T> func, string order)
+	{
+		return list.OrderBy<T>(i => func(i));
+	}
+	public static IEnumerable<int> OrderBy(this IEnumerable<int> list,
+		Func<int, int> sortFunction, string order)
+	{
+		if (order == "desc")
+		{
+			return list.OrderByDescending(c => sortFunction(c));
+		}
+		return list.OrderBy(c => sortFunction(c));
+
+	}
+
+	public static IEnumerable<int> ApplyMethod(this IEnumerable<int> list,
+		Func<int, int> fun)
+	{
+		return list.Select(x => fun(x));
+	}
+}
